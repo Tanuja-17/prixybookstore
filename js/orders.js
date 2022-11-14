c9ecf3e7fafad8bc91fc757cacefc34d8b13048e
Tanuja-17 <91649438+Tanuja-17@users.noreply.github.com>
2022-11-14 14:19:09 +0800

    initial

---
+++ js/orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'SPACE ACADEMY',
+        productNumber: '66321',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'EYES THAT SPEAK OF THE STARS',
+        productNumber: '88921',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'THE GUARDIAN OF THE REALM',
+        productNumber: '66542',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'THE GOING TO BED BOOK',
+        productNumber: '68561',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
